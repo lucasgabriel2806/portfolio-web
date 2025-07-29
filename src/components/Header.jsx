458fc2b07738f1d28de422dd07b1ea0c7df44afe
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-29 10:41:47 -0300

    Atualizado header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,53 @@ function Header() {
   return (
 
     <header className='fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0'>
-        Header
+
+        <div className='max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr_3fr_1fr]'>
+
+          <h1>
+
+            <a 
+            href='/'
+            className='logo'
+            >
+
+              <img
+              src='/images/logo.svg' 
+              width={40}
+              height={40}
+              alt='Lucas de Paula' 
+              />
+
+            </a>
+
+          </h1>
+
+          <div className='relative md:justify-self-center'>
+
+            <button
+            className='menu-btn md:hidden'
+            onClick={null}
+            >
+
+              <span class='material-symbols-rounded'>
+                menu
+              </span>
+
+            </button>
+
+            Navbar
+
+          </div>
+
+          <a 
+            href='#contact' 
+            className=''
+          >
+            Contact Me
+          </a>
+
+        </div>
+
     </header>
 
   )
